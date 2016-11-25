ffa3c95037c7f3bb9fbe25a1e6ea228bbd3de73f
xxjcaxx <xxjcaxx>
2016-11-25 16:57:51 +0000

    progres admin

--- admin/clients.sh
+++ admin/clients.sh
@@ -1,11 +1,13 @@
-nmap -sP 192.168.8.0/24 > /tmp/clients
-cat /tmp/clients | egrep -o '192.168.8.[0-9]+' > /tmp/ips
+xarxa=$(ifconfig | grep -o '192.168.[0-9]*.254' | cut -d'.' -f1-3)
+
+nmap -sP ${xarxa}.0/24 > /tmp/clients
+cat /tmp/clients | egrep -o ${xarxa}'.[0-9]+' > /tmp/ips
 
 echo '<ul>' > clients.html
 while read ip
 do
 
-echo '<li id="c'${ip}'">'$ip'</li>' >> clients.html
+echo '<li id="c'${ip}'"><span>'$ip'</span></li>' >> clients.html
 
 done < /tmp/ips
 
--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,17 +1,15 @@
-
 $(document).on("click","#act_clients",function(event){
 $("#panel").html('<img src="images/loading.gif"/>');
 
 $.get('pcs.php',{ opt: "actualitzar" },function(data){
 $("#panel").html("<span>Llista de clients:</span>");
 $("#panel").append(data);
-}).done(funtion(){ 
-$("#panel").append("sadf");
-
+}).done(function(){                             
+f_pcs();
+}); 
 });
 
 
-});
 
 $(function()
 {
@@ -23,6 +21,21 @@ $("#actual").append(data);
 $.get('pcs.php',function(data){
 $("#panel").html("<span>Llista de clients:</span>");
 $("#panel").append(data);
+}).done(function(){
+//console.log($("#panel ul li"));
+//$("#panel ul li").css('color','red');
+f_pcs();
 });
 
 });
+
+
+
+
+function f_pcs(){
+
+
+$("#panel ul li").each(function(i){
+$(this).append('<img alt="Bloquea esta IP" src="images/connected.png"/>');
+});
+}
