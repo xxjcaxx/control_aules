c54e5bcb66d63a08bf924e9e1df4702631b798b5
xxjcaxx <xxjcaxx@yahoo.es>
2017-02-16 10:26:10 +0100

    escala a kb

--- admin/instalacion/actualitzar.sh
+++ admin/instalacion/actualitzar.sh
@@ -12,7 +12,7 @@ read cip cin cout <<< $line
 #echo $line $cip $cin $cout
 cip=$(echo $cip | cut -d"." -f4)
 rrdtool update /var/lib/control_aules/client$cip.rrd $(date +%s):$cin:$cout
-rrdtool graph /var/lib/control_aules/spd$cip.png --start -6h --end $(date +%s) DEF:sin=/var/lib/control_aules/client$cip.rrd:in:AVERAGE LINE2:sin\#FF0000:"in" DEF:sout=/var/lib/control_aules/client$cip.rrd:out:AVERAGE LINE2:sout\#00FF00:"out"
+rrdtool graph /var/lib/control_aules/spd$cip.png --start -6h --end $(date +%s) DEF:sin=/var/lib/control_aules/client$cip.rrd:in:AVERAGE CDEF:kbin=sin,1024,\/ LINE1:kbin\#FF0000:"in" DEF:sout=/var/lib/control_aules/client$cip.rrd:out:AVERAGE CDEF:kbout=sout,1024,\/ LINE1:kbout\#00FF00:"out"
 done
 
 args=""
@@ -22,7 +22,7 @@ do
 ip=$(echo $ip | cut -d"." -f4)
 color=$(($ip%64+1))
 color=$(echo "$colors" | sed -n "$color p")
-[[ $ip -lt 200 ]] && args=$args" DEF:sin$ip=/var/lib/control_aules/client$ip.rrd:in:AVERAGE LINE1:sin$ip$color:\"192.168.x.$ip\""
+[[ $ip -lt 200 ]] && args=$args" DEF:sin$ip=/var/lib/control_aules/client$ip.rrd:in:AVERAGE CDEF:kbin$ip=sin$ip,1024,/ LINE1:kbin$ip$color:\"192.168.x.$ip\""
 done < /tmp/ips
 
 rrdtool graph /var/lib/control_aules/total.png --start -6h --end $(date +%s) $args
