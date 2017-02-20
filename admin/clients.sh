7a55275989e5b6295dd1f983a944db48da8ad65a
xxjcaxx <xxjcaxx@yahoo.es>
2017-02-20 08:43:44 +0100

    mes clients

--- admin/clients.sh
+++ admin/clients.sh
@@ -1,9 +1,13 @@
-sudo chmod 666 clients.html
+#sudo chmod 666 clients.html
 #> alumnes
 xarxa=$(ifconfig | grep -o '192.168.[0-9]*.254' | cut -d'.' -f1-3)
 
 nmap -sP ${xarxa}.0/24 > /tmp/clients
-cat /tmp/clients | egrep -o ${xarxa}'.[0-9]+' > /tmp/ips
+arp -a -n | grep -v 'incomplete' |egrep -o '192.168.[0-9]+.[0-9]+' >> /tmp/clients
+
+
+
+cat /tmp/clients | egrep -o ${xarxa}'.[0-9]+' | sort -V | uniq  > /tmp/ips
 
 #echo '<ul>' > clients.html
 #while read ip
