5e193634dba3338810203c441b455da11270a050
xxjcaxx <xxjcaxx@yahoo.es>
2017-03-31 17:36:01 +0200

    caputrar4

--- admin/capturar.php
+++ admin/capturar.php
@@ -4,7 +4,7 @@ if($_SESSION['user']=='lliurex'){
 if(isset($_GET['ip'])){
 $prefijo=exec("ifconfig | egrep -o '192.168.[0-9]+'");
            
-$res = exec('sudo ssh  -o "StrictHostKeyChecking no" root@'.$prefijo.'.'.$_GET['ip'].' \'bash -s\' < ./scripts/captures.sh > /var/lib/control_aules/captura'.$_GET['ip'].'.png' );
+$res = exec('sudo ssh -o "ConnectTimeout=10" -o "StrictHostKeyChecking no" root@'.$prefijo.'.'.$_GET['ip'].' \'bash -s\' < ./scripts/captures.sh > /var/lib/control_aules/captura'.$_GET['ip'].'.png' );
            echo $res;
            
 	}
