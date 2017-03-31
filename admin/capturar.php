b2b1d976cb4083c400b32d4cdd80c00baeac237b
xxjcaxx <xxjcaxx@yahoo.es>
2017-03-31 17:19:54 +0200

    capturar2

--- admin/capturar.php
+++ admin/capturar.php
@@ -2,7 +2,7 @@
 session_start();
 if($_SESSION['user']=='lliurex'){
 if(isset($_GET['ip'])){
-           $res = exec('sudo ssh root@192.168.9.'.$_GET['ip'].' \'bash -s\' < ./scripts/captures.sh > /var/lib/control_aules/captura'.$_GET['ip'].'.png' );
+           $res = exec('sudo ssh  -o "StrictHostKeyChecking no" root@192.168.9.'.$_GET['ip'].' \'bash -s\' < ./scripts/captures.sh > /var/lib/control_aules/captura'.$_GET['ip'].'.png' );
            echo $res;
            
 	}
