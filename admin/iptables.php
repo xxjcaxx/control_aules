c497d001caead9879528eab657c2b6c752169190
xxjcaxx <xxjcaxx@yahoo.es>
2017-10-24 10:33:29 +0200

    fallos en iptables

--- admin/iptables.php
+++ admin/iptables.php
@@ -2,8 +2,11 @@
 <?php
 //echo "\n---------------- GENERAL ---------------\n";
 //system('sudo iptables -L');
+echo "\n---------------- Route -n ---------------\n";
+system('sudo route -n');
+
 echo "\n---------------- NAT ---------------\n";
-system('sudo iptables -t nat -L');
+system('sudo iptables -t nat -L -v');
 
 echo "\n---------------- IPtables-save ---------------\n";
 system('sudo iptables-save | grep -v ACCT'); //llevar els de comptabilitat
