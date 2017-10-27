ff5da42e55d15f8068a3dcb960ecd5305442a360
xxjcaxx <xxjcaxx@yahoo.es>
2017-10-27 18:48:51 +0200

    ralentir no talla

--- admin/reset.php
+++ admin/reset.php
@@ -4,6 +4,7 @@ session_start();
 if($_SESSION['user']=='lliurex'){
 
 $res = exec('sudo ip route change default via 10.20.0.1 dev eth0');
+$res = exec('sudo ip route change 10.20.0.0/16 dev eth0');
 $res = exec('sudo wondershaper clear eth2');
 }
 ?>
