a450605f5cbc4802683cd80addf6938bf3b53720
xxjcaxx <xxjcaxx@yahoo.es>
2017-11-21 10:32:03 +0100

    proves2

--- admin/scripts/capturartots.sh
+++ admin/scripts/capturartots.sh
@@ -7,6 +7,7 @@ while read ip
 do
 	if echo $ip | grep '192.168.[0-9]*.1[0-2][0-9]' > /dev/null
 	then
+		ip=$(echo $ip | cut -d'.' -f4)
 		echo "capturant $ip"
 		sudo ssh -o "ConnectTimeout=10" -o "StrictHostKeyChecking no" root@${ip} 'bash -s' < /var/www/html/admin/scripts/captures.sh > /var/lib/control_aules/captura${ip}.jpg
          fi
